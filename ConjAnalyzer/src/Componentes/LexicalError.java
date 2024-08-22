2f5c1181be1031d056fff60ed6ce5d6f99437d93
Keneth Lopez <3116549240704@ingenieria.usac.edu.gt>
2024-08-22 13:10:21 -0600

    Se agrega clase que define un Error Lexico

---
+++ ConjAnalyzer/src/Componentes/LexicalError.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Componentes;
+
+/**
+ *
+ * @author Keneth Lopez
+ */
+public class LexicalError {
+    private String valor;
+    private String mensaje;
+    private int linea;
+    private int columna;
+
+    public LexicalError(String valor, String mensaje, int linea, int columna) {
+        this.valor = valor;
+        this.mensaje = mensaje;
+        this.linea = linea;
+        this.columna = columna;
+    }
+
+    // Getters y Setters
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public int getLinea() {
+        return linea;
+    }
+
+    public void setLinea(int linea) {
+        this.linea = linea;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
+}
