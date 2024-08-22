52e01c31bb3932521b74dd2d3d2c5d2e1cb06132
Keneth Lopez <3116549240704@ingenieria.usac.edu.gt>
2024-08-22 13:10:10 -0600

    Se agrega clase que define un Token

---
+++ ConjAnalyzer/src/Componentes/Token.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Componentes;
+
+/**
+ *
+ * @author Keneth Lopez
+ */
+public class Token {
+    private String tipo;
+    private String valor;
+    private int linea;
+    private int columna;
+
+    public Token(String tipo, String valor, int linea, int columna) {
+        this.tipo = tipo;
+        this.valor = valor;
+        this.linea = linea;
+        this.columna = columna;
+    }
+
+    // Getters y Setters
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public int getLinea() {
+        return linea;
+    }
+
+    public void setLinea(int linea) {
+        this.linea = linea;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
+}
