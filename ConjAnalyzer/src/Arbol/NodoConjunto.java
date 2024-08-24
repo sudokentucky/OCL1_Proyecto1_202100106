d2e633df90c0a31ae7be6fc06a37e5c7a3717aa9
Keneth Lopez <3116549240704@ingenieria.usac.edu.gt>
2024-08-23 18:03:14 -0600

    Se modifica la clase que define el nodo de un conjunto

--- ConjAnalyzer/src/Arbol/NodoConjunto.java
+++ ConjAnalyzer/src/Arbol/NodoConjunto.java
@@ -4,20 +4,14 @@
  */
 package Arbol;
 
-import java.util.Set;
 import Conjuntos.ConjuntoManager;
-import Conjuntos.GestorOperaciones;
+import java.util.Set;
 
 /**
  *
  * @author Keneth Lopez
  */
-abstract class Nodo {
-    abstract Set<Character> evaluar();
-    abstract String mostrarContenido();
-}
-
-class NodoConjunto extends Nodo {
+public class NodoConjunto extends Nodo {
     private String nombreConjunto;
     private ConjuntoManager conjuntoManager;
 
@@ -58,72 +52,3 @@ class NodoConjunto extends Nodo {
         return nombreConjunto.hashCode();
     }
 }
-
-
-class NodoOperacion extends Nodo {
-    private String operador;
-    private Nodo izquierdo;
-    private Nodo derecho;
-
-    public NodoOperacion(String operador, Nodo izquierdo, Nodo derecho) {
-        this.operador = operador;
-        this.izquierdo = izquierdo;
-        this.derecho = derecho;
-    }
-
-    @Override
-    public Set<Character> evaluar() {
-        Set<Character> resultadoIzquierdo = izquierdo.evaluar();
-        Set<Character> resultadoDerecho = derecho != null ? derecho.evaluar() : null;
-
-        switch (operador) {
-            case "U":
-                return GestorOperaciones.union(resultadoIzquierdo, resultadoDerecho);
-            case "&":
-                return GestorOperaciones.interseccion(resultadoIzquierdo, resultadoDerecho);
-            case "-":
-                return GestorOperaciones.diferencia(resultadoIzquierdo, resultadoDerecho);
-            case "^":
-                return GestorOperaciones.complemento(resultadoIzquierdo);
-            default:
-                throw new IllegalArgumentException("Operador desconocido: " + operador);
-        }
-    }
-
-    @Override
-    public String mostrarContenido() {
-        String contenidoIzquierdo = izquierdo.mostrarContenido();
-        String contenidoDerecho = derecho != null ? derecho.mostrarContenido() : "";
-
-        switch (operador) {
-            case "U":
-                return "(" + contenidoIzquierdo + " U " + contenidoDerecho + ")";
-            case "&":
-                return "(" + contenidoIzquierdo + " & " + contenidoDerecho + ")";
-            case "-":
-                return "(" + contenidoIzquierdo + " - " + contenidoDerecho + ")";
-            case "^":
-                return "^" + contenidoIzquierdo;
-            default:
-                throw new IllegalArgumentException("Operador desconocido: " + operador);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return mostrarContenido();
-    }
-
-    public String getOperador() {
-        return operador;
-    }
-
-    public Nodo getIzquierdo() {
-        return izquierdo;
-    }
-
-    public Nodo getDerecho() {
-        return derecho;
-    }
-}
-
