1683a8b60c9e369dc7289a2c92ed775dbacb6461
Keneth Lopez <3116549240704@ingenieria.usac.edu.gt>
2024-08-23 18:34:08 -0600

    Se agrega un método para generar el reporte en un html

--- ConjAnalyzer/src/Componentes/Token.java
+++ ConjAnalyzer/src/Componentes/Token.java
@@ -3,11 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Componentes;
-
 /**
  *
  * @author Keneth Lopez
  */
+import java.util.List;
+
 public class Token {
     private String tipo;
     private String valor;
@@ -52,5 +53,26 @@ public class Token {
 
     public void setColumna(int columna) {
         this.columna = columna;
+    }
+
+    // Método estático para generar la tabla de tokens en formato HTML
+    public static String generarTablaHTML(List<Token> tokens) {
+        StringBuilder html = new StringBuilder();
+        html.append("<html><body>");
+        html.append("<table border='1'>");
+        html.append("<tr><th>Tipo</th><th>Valor</th><th>Línea</th><th>Columna</th></tr>");
+
+        for (Token token : tokens) {
+            html.append("<tr>");
+            html.append("<td>").append(token.getTipo()).append("</td>");
+            html.append("<td>").append(token.getValor()).append("</td>");
+            html.append("<td>").append(token.getLinea()).append("</td>");
+            html.append("<td>").append(token.getColumna()).append("</td>");
+            html.append("</tr>");
+        }
+
+        html.append("</table>");
+        html.append("</body></html>");
+        return html.toString();
     }
 }
