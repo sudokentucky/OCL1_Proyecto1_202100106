ed36b21d4181b955470eb49b5bd0e5661f8c0bc8
Keneth Lopez <3116549240704@ingenieria.usac.edu.gt>
2024-08-23 18:02:47 -0600

    Se agrega la clase abstracta de un nodo

---
+++ ConjAnalyzer/src/Arbol/Nodo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Arbol;
+import java.util.Set;
+/**
+ *
+ * @author Keneth Lopez
+ */
+
+public abstract class Nodo {
+    public abstract Set<Character> evaluar();
+    public abstract String mostrarContenido();
+}
