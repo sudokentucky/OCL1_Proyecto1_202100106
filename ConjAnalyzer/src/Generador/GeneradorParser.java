c7ebd68ceb880a1fe66cca7d17c9a26ec0a6dd84
Keneth Lopez <3116549240704@ingenieria.usac.edu.gt>
2024-08-23 18:02:35 -0600

    Se agrega un generador para el parser

---
+++ ConjAnalyzer/src/Generador/GeneradorParser.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Generador;
+
+/**
+ *
+ * @author Keneth Lopez
+ */
+public class GeneradorParser {
+    public static void main(String[] args) {
+        try {
+            // Ruta donde está el archivo de CUP
+            String rutaEspecificaciones = "./src/Language/";
+
+            // Ruta donde se generarán los archivos .java
+            String rutaAnalizadores = "./src/Analizadores/";
+
+            // Convertir a rutas absolutas para evitar problemas de rutas relativas
+            String rutaEspecificacionesAbsoluta = new java.io.File(rutaEspecificaciones).getAbsolutePath();
+            String rutaAnalizadoresAbsoluta = new java.io.File(rutaAnalizadores).getAbsolutePath();
+
+            // Generar el analizador sintáctico con CUP
+            String[] opCUP = {"-destdir", rutaAnalizadoresAbsoluta, "-parser", "Parser", rutaEspecificacionesAbsoluta + "/Parser.cup"};
+            java_cup.Main.main(opCUP);
+            System.out.println("Generación de analizador sintáctico completada.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
